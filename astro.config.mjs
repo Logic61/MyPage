2c69b15415c2582eff8e5e02b4860bbeabaa6e24
logic <logic.dreemurr@gmail.com>
2026-03-31 23:48:32 +0800

    Fix github pages assets and links by setting site and base in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import rehypeMathjax from 'rehype-mathjax/svg';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://logic61.github.io',
+  base: '/MyPage',
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeMathjax],
